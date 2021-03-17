155268919cb6e1fac4818bdf5f8696cc887c6870
tydshun <69792117+tydshun@users.noreply.github.com>
2021-03-17 16:30:45 +0900

    テーブルの記述変更

--- db/schema.rb
+++ db/schema.rb
@@ -20,7 +20,6 @@ ActiveRecord::Schema.define(version: 2021_03_17_064801) do
   end
 
   create_table "profiles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "nickname", null: false
     t.string "first_name", null: false
     t.string "family_name", null: false
     t.integer "phone_number", null: false
@@ -33,6 +32,7 @@ ActiveRecord::Schema.define(version: 2021_03_17_064801) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
