c4b7cfd479ea3f1dc54ab5d640fc5cd9a021649c
tydshun <69792117+tydshun@users.noreply.github.com>
2021-03-19 18:33:28 +0900

    Calendarのトップページの作成

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,4 +1,5 @@
 class BlogsController < ApplicationController
+
   def index
     @blogs = Blog.all
   end
@@ -40,4 +41,5 @@ class BlogsController < ApplicationController
   def blog_parameter
     params.require(:blog).permit(:title, :content, :start_time)
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :blogs
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "posts#index"
+
+get 'blogs/index'
+  resources :blogs
 end
