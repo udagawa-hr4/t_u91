492fab0ca326afa28d07b221d8486397cfb41716
udagawa-hr4 <hiro619881022@gmil.com>
2021-03-23 12:32:39 +0900

    詳細ページの大枠のビューの設定(コメント機能実装前)

--- app/javascript/packs/main.js
+++ app/javascript/packs/main.js
@@ -60,42 +60,42 @@ class LinkMenu {
 
 new LinkMenu();
 
-class TextChange{
-  constructor(){
-    this.DOM = {};
-    this.DOM.btnTweet = document.querySelector(".btn-label");
-    this.DOM.btnImg = document.querySelector(".img-label")
-    this._event();
-  }
-  _toggleBtn(){
-      this.DOM.btnTweet.classList.toggle(".hover-btn")
-      this.DOM.btnImg.classList.toggle(".hover-btn")
-  }
-  _changeIntTw(){
-      this.DOM.btnTweet.innerHTML = ("投稿");
+// class TextChange{
+//   constructor(){
+//     this.DOM = {};
+//     this.DOM.btnTweet = document.querySelector(".btn-label");
+//     this.DOM.btnImg = document.querySelector(".img-label")
+//     this._event();
+//   }
+//   _toggleBtn(){
+//       this.DOM.btnTweet.classList.toggle(".hover-btn")
+//       this.DOM.btnImg.classList.toggle(".hover-btn")
+//   }
+//   _changeIntTw(){
+//       this.DOM.btnTweet.innerHTML = ("投稿");
 
-    }
-  _changeIntRvTw(){
-      this.DOM.btnTweet.innerHTML = ("Tweet");
+//     }
+//   _changeIntRvTw(){
+//       this.DOM.btnTweet.innerHTML = ("Tweet");
       
-  }
-  _changeIntImg(){
-    this.DOM.btnImg.innerHTML = ("画像を選択");
-    }
-  _changeIntRvImg(){
-    this.DOM.btnImg.innerHTML = ("Upload data");
-    }
+//   }
+//   _changeIntImg(){
+//     this.DOM.btnImg.innerHTML = ("画像を選択");
+//     }
+//   _changeIntRvImg(){
+//     this.DOM.btnImg.innerHTML = ("Upload data");
+//     }
 
-  _event(){
-      this.DOM.btnTweet.addEventListener("mouseover",this._toggleBtn.bind(this));
-      this.DOM.btnTweet.addEventListener("mouseover",this._changeIntTw.bind(this));
-      this.DOM.btnTweet.addEventListener("mouseout",this._toggleBtn.bind(this));
-      this.DOM.btnTweet.addEventListener("mouseout",this._changeIntRvTw.bind(this));
-      this.DOM.btnImg.addEventListener("mouseover",this._toggleBtn.bind(this));
-      this.DOM.btnImg.addEventListener("mouseover",this._changeIntImg.bind(this));
-      this.DOM.btnImg.addEventListener("mouseout",this._toggleBtn.bind(this));
-      this.DOM.btnImg.addEventListener("mouseout",this._changeIntRvImg.bind(this));
+//   _event(){
+//       this.DOM.btnTweet.addEventListener("mouseover",this._toggleBtn.bind(this));
+//       this.DOM.btnTweet.addEventListener("mouseover",this._changeIntTw.bind(this));
+//       this.DOM.btnTweet.addEventListener("mouseout",this._toggleBtn.bind(this));
+//       this.DOM.btnTweet.addEventListener("mouseout",this._changeIntRvTw.bind(this));
+//       this.DOM.btnImg.addEventListener("mouseover",this._toggleBtn.bind(this));
+//       this.DOM.btnImg.addEventListener("mouseover",this._changeIntImg.bind(this));
+//       this.DOM.btnImg.addEventListener("mouseout",this._toggleBtn.bind(this));
+//       this.DOM.btnImg.addEventListener("mouseout",this._changeIntRvImg.bind(this));
 
-  }
-}
- new TextChange();
+//   }
+// }
+//  new TextChange();
