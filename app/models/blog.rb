190a8a7acac225a40fd84db4371bb36ce4d9938f
tydshun <69792117+tydshun@users.noreply.github.com>
2021-03-30 15:46:39 +0900

    モデルの修正

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,6 +1,8 @@
 class Blog < ApplicationRecord
   validates :title, presence: true
 
+  # belongs_to :user
+
   def self.search(search)
     if search
       Blog.where(['content LIKE ?', "%#{search}%"])
